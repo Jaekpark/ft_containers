ba2e6871e8cb3554c4bd46b9a86e2bd879107d79
Jaekpark <xhxkf123@gmail.com>
2022-05-08 19:39:05 +0900

    Write vector.hpp
    member type definition

--- srcs/Vector.hpp
+++ srcs/Vector.hpp
@@ -6,6 +6,7 @@
 #include <memory>
 #include <vector>
 #include <iterator>
+// #include <iterator/iterator_traits.h>
 #include <stdexcept>
 
 namespace ft {
@@ -13,17 +14,46 @@ namespace ft {
 	template < class T, class Allocator = std::allocator<T> >
 	class Vector {
 	public:
-		typedef T			value_type;
-		typedef Allocator	allocator_type;
-		typedef typename allocator_type::reference  reference;
-		typedef typename allocator_type::const_reference  const_reference;
-		typedef typename std::iterator
+		typedef T											value_type;
+		typedef Allocator									allocator_type;
+		typedef typename allocator_type::size_type			size_type;
+		// typedef typename std::allocator_traits<allocator_type>::size_type			size_type;
+	// protected:
+		typedef typename allocator_type::reference			reference;
+		typedef typename allocator_type::const_reference	const_reference;
+        // typedef std::allocator_traits<allocator_type>::reference    reference;
+        // typedef std::allocator_traits<allocator_type>::const_reference    const_reference;
+		typedef std::iterator<allocator_type>				iterator;
+		typedef std::iterator<const_allocator_type>			const_iterator;
+		typedef typename allocator_type::difference_type	difference_type;
+		typedef typename allocator_type::pointer			pointer;
+		typedef typename allocator_type::const_pointer		const_pointer;
+		// typedef typename std::allocator_traits<allocator_type>::difference_type	difference_type;
+		// typedef typename std::allocator_traits<allocator_type>::pointer			pointer;
+		// typedef typename std::allocator_traits<allocator_type>::const_pointer		const_pointer;
+		typedef typename std::reverse_iterator<iterator>				reverse_iterator;
+		typedef typename std::reverse_iterator<const_iterator>		const_reverse_iterator;
 
-		protected:
+		Vector(void);
+		explicit Vector(const allocator_type& alloc = allocator_type());
+		// explicit vector(size_type n, const allocator_type& alloc = allocator_type()) {
+			
+		// }
+		// explicit vector(size_type n, const value_type& value = value_type(), const allocator_type& alloc = allocator_type()) {
+		// 	for (int i = 0; i < n; i++)
+		// 		pointer++ = value;
+		// }
+		// template <class InputIterator>
+		// 	explicit vector(InputIterator first, Inputiterator last, const allocator_type& alloc = allocator_type()) {
+		// 		for (iterator iter = first; iter != last; first++)
+		// 			iterator 
+		// 	}
+		// explicit Vector(const vector& x);
+		~Vector(void);
 
-		private:
-	}
+	private:
+
+	};
 }
-class Vector
 
 #endif
--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,23 +1,8 @@
 #include <iostream>
-#include <vector>
-#include <memory>
-
-void	print(std::vector<int>& target) {
-	std::vector<int>::iterator begin = target.begin();
-	std::cout << "size : " << target.size()
-		<< ", capacity : " << target.capacity() << std::endl;
-	while (begin != target.end())
-		std::cout << *(begin++) << " ";
-	std::cout << std::endl;
-}
+#include "Vector.hpp"
 
 int main(void) {
-	std::vector<int> a(3, 1);
-	std::vector<int> b(1, 11);
+	ft::Vector<int> temp;
 
-	a.push_back(12);
-	print(a);
-	print(b);
-	std::cout << (a < b) << std::endl;
 	return 0;
 }
