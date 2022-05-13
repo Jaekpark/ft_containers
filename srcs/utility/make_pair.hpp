1992f63bf55a0c5b848668f8ffed4e13c5bc5e85
Jaekpark <xhxkf123@gmail.com>
2022-05-13 19:16:18 +0900

    Declare pair, make_pair

---
+++ srcs/utility/make_pair.hpp
@@ -0,0 +1,14 @@
+#ifndef _FT_MAKE_PAIR_HPP
+#define _FT_MAKE_PAIR_HPP
+
+#include "../config.hpp"
+#include "../utility/pair.hpp"
+
+//namespace_start
+_BEGIN_NAMESPACE_FT
+
+template <class T1, class T2>
+	pair<T1, T2> make_pair(T1 x, T2 y) {};
+//namespace_end
+_END_NAMESPACE_FT
+#endif
---
+++ srcs/utility/pair.hpp
@@ -0,0 +1,30 @@
+#ifndef _FT_PAIR_HPP
+#define _FT_PAIR_HPP
+
+#include "../config.hpp"
+//namespace_start
+_BEGIN_NAMESPACE_FT
+
+template <class T1, class T2>
+struct pair {
+protected:
+
+private:
+
+public:
+	//default_constructor
+	explicit pair(void);
+	//initialize
+	explicit pair(const T1& x, const T2& y);
+	//copy_constructor
+	template <class U1, class U2>
+		pair(const pair<U1, U2>& p) {};
+	//destructor
+	~pair(void);
+	//operator=
+	pair operator=(const pair& x);
+};
+//namespace_end
+_END_NAMESPACE_FT
+
+#endif
