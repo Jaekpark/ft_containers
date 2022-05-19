27224cf54fd6188dddf9d7098c94a642cfa72ced
Jaekpark <xhxkf123@gmail.com>
2022-05-20 00:01:25 +0900

    Implement iterator (#6)
    write member functions
    write operator overloads

--- srcs/iterator/iterator.hpp
+++ srcs/iterator/iterator.hpp
@@ -35,13 +35,90 @@ class wrap_iterator {
   iterator_type _i;
 
  public:
-  wrap_iterator(void) : _i{};
-  wrap_iterator(const wrap_iterator<Iterator>)
-};
-/** c++ 20 부터는 인접 반복자(contiguous_iterator)가 추가되었습니다.
- * struct contiguous_iterator_tag	: public random_access_iterator_tag {};
- **/
+  wrap_iterator(void) : _i {}
+  explicit wrap_iterator(const wrap_iterator& x) : _i(x.base()) {}
+  wrap_iterator(const wrap_iterator<U>& u) : current(u.base()) {}
+  wrap_iterator operator=(const wrap_iterator& x) {
+    current = x.base();
+    return *this;
+  }
 
-_END_NAMESPACE_FT
+  iterator_type base(void) { return _i; }
+  reference operator*(void) const { return *_i; }
+  pointer operator->(void) const { return &(operator*()); }
+  wrap_iterator& operator++() {
+    ++_i;
+    return *this;
+  }
+  wrap_iterator operator++(int) {
+    wrap_iterator tmp(*this);
+    ++(*this);
+    return tmp;
+  }
+  wrap_iterator& operator--(void) {
+    --_i;
+    return *this;
+  }
+  wrap_iterator operator--(int) {
+    wrap_iterator tmp(*this);
+    --(*this);
+    return tmp;
+  }
+  wrap_iterator operator+(difference_type n) const {
+    wrap_iterator iter(*this);
+    iter += n;
+    return iter;
+  }
+  wrap_iterator& operator+=(difference_type n) {
+    _i += n;
+    return *this;
+  }
+  wrap_iterator operator-(difference_type n) { return *this + (-n); }
+  wrap_iterator& operator-=(difference_type n) {
+    *this += -n;
+    return *this;
+  }
+  reference operator[](difference_type n) { return _i[n]; }
+  wrap_iterator<Iterator> operator+(
+      typename wrap_iterator<Iterator>::difference_type n,
+      wrap_iterator<Iterator> x) {
+    x += n;
+    return x;
+  }
+  difference_type operator-(const wrap_iterator<Iterator>& x,
+                            const wrap_iterator<Iterator>& y) {
+    return x.base() - y.base();
+  }
+  bool operator==(const wrap_iterator<Iterator>& x,
+                  const wrap_iterator<Iterator>& y) {
+    return x.base() == y.base();
+  }
+
+  bool operator<(const wrap_iterator<Iterator>& x,
+                 const wrap_iterator<Iterator>& y) {
+    return x.base() < y.base();
+  }
+  bool operator!=(const wrap_iterator<Iterator>& x,
+                  const wrap_iterator<Iterator>& y) {
+    return !(x == y);
+  }
+  bool operator>(const wrap_iterator<Iterator>& x,
+                 const wrap_iterator<Iterator>& y) {
+    return y < x;
+  }
+  bool operator>=(const wrap_iterator<Iterator>& x,
+                  const wrap_iterator<Iterator>& y) {
+    return !(x < y);
+  }
+  bool operator<=(const wrap_iterator<Iterator>& x,
+                  const wrap_iterator<Iterator>& y) {
+    return !(y < x);
+  }
+
+  /** c++ 20 부터는 인접 반복자(contiguous_iterator)가 추가되었습니다.
+   * struct contiguous_iterator_tag	: public random_access_iterator_tag {};
+   **/
+
+  _END_NAMESPACE_FT
 
 #endif
