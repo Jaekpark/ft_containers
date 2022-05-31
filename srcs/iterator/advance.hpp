3948dff73dd4c07d0f1916843ff9c4d59a8d76a4
Jaekpark <xhxkf123@gmail.com>
2022-05-31 13:01:39 +0900

    Write advance (#6)

--- srcs/iterator/advance.hpp
+++ srcs/iterator/advance.hpp
@@ -5,10 +5,28 @@
 #include "../iterator/iterator.hpp"
 
 _BEGIN_NAMESPACE_FT
-template <class InputIterator, class Distance>
-	void	advance(InputIterator& i, Distance n) {
-		for (;)
-	}
+template <class InputIterator>
+void advance(InputIterator& i,
+             typename iterator_traits<InputIterator>::difference_type n,
+             input_iterator_tag) {
+  for (; n > 0; --n) ++i;
+}
 
+template <class BidirIterator>
+void advance(BidirIterator& i,
+             typename iterator_traits<BidirIterator>::difference_type n,
+             bidirectional_iterator_tag) {
+  if (n >= 0)
+    for (; n > 0; --n) ++i;
+  else
+    for (; n < 0; ++n) --i;
+}
+
+template <class RandomIterator>
+void advance(RandomIterator& i,
+             typename iterator_traits<RandomIterator>::difference_type n,
+             random_access_iterator_tag) {
+  i += n;
+}
 _END_NAMESPACE_FT
 #endif
